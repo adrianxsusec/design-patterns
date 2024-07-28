02d56108dfaadd6e4ecf49ba790f6d701000b79a
adrianxsusec <adrian.susec@gmail.com>
2024-07-28 19:46:16 +0200

    format

--- principles-strategy-observer/upgrade-without-change.cpp
+++ principles-strategy-observer/upgrade-without-change.cpp
@@ -7,7 +7,7 @@ Iterator mymax(
 
     Iterator max_iter = first;
     first++;
-    
+
     while (first != last) {
         if (pred(first, max_iter) == true) {
             max_iter = first;
@@ -22,10 +22,12 @@ template <typename Iterator> bool gt_int(Iterator first, Iterator second){
 }
 
 int arr_int[] = { 1, 3, 5, 7, 4, 6, 9, 2, 0 };
+
 int main(){
   const int* maxint = mymax(&arr_int[0], &arr_int[sizeof(arr_int)/sizeof(*arr_int)], gt_int<const int*>);
   if (maxint == nullptr) {
+    std::cout << "Array is empty!" << "\n";
     exit(0);
   }
-  std::cout <<*maxint <<"\n";
+  std::cout << *maxint << "\n";
 }
